c655d719b424ccf4932e313ad8fa3439d12757d2
zulkharnain-brodersen <sde@brodersen.com>
2026-05-25 16:59:35 +0530

    Game Dashboard Partially Complete

---
+++ game_js/game.js
@@ -0,0 +1,63 @@
+// Game Dashboard JavaScript (Audio and Game List)
+let gamesList = [
+  "Snake Game",
+  "Tic Tac Toe Game",
+  "Memory Game",
+  "Pong Game",
+  "Minesweeper Game",
+];
+let dashboardAudio = new Audio("./assets/game_dashboard.mp3");
+let gameItemAudio = new Audio("./assets/option.wav");
+let gameVideoPath = "./assets/dashboard.mp4";
+
+dashboardAudio.loop = true;
+dashboardAudio.volume = 0.5;
+dashboardAudio.preload = "auto";
+dashboardAudio.load();
+
+function startAudio() {
+  dashboardAudio.play();
+}
+
+document.addEventListener("click", startAudio, { once: true });
+document.addEventListener("keydown", startAudio, { once: true });
+
+//  List of Games and their corresponding HTML files
+
+let gameContainer = document.getElementById("game-container");
+
+let ul = document.createElement("ul");
+gamesList.forEach((game) => {
+  let li = document.createElement("li");
+  li.setAttribute("class", "game-item");
+  li.innerText = game;
+  li.addEventListener("click", () => {
+    window.location.href = `${game.trim().split(" ").join("_")}/index.html`;
+  });
+  ul.appendChild(li);
+});
+gameContainer.appendChild(ul);
+
+// game item hover effect
+let gameItems = document.querySelectorAll(".game-item");
+gameItems.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    gameItemAudio.currentTime = 0; // Reset audio to start
+    gameItemAudio.play();
+  });
+  item.addEventListener("mouseout", () => {
+    gameItemAudio.pause();
+    gameItemAudio.currentTime = 0; // Reset audio to start
+  });
+});
+
+let gameVideo = document.getElementById("game-video");
+
+let video = document.createElement("video");
+
+video.src = gameVideoPath;
+video.controls = false;
+video.autoplay = true;
+video.muted = true; // needed for autoplay in browsers
+
+gameVideo.appendChild(video);
