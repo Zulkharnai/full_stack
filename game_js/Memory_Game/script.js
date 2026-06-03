4695ca6ba761b77ca60d3617c13b6c46cd3057f0
zulkharnain-brodersen <sde@brodersen.com>
2026-06-03 12:09:31 +0530

    show modal

--- game_js/Memory_Game/script.js
+++ game_js/Memory_Game/script.js
@@ -1,13 +1,19 @@
 const gameImages = document.querySelectorAll(".game-img");
 const scoreDisplay = document.getElementById("score");
+const finalScoreDisplay = document.getElementById("final-score");
+const resetButton = document.getElementById("restart");
+const dialog = document.getElementById("dialog");
+let closeDialogBtn = document.getElementById("dialog-close");
+
 let store = [];
 let clickCount = 0;
 let score = 0;
+scoreDisplay.textContent = `Score: ${score}`;
 
 function continueGame() {
   let random = Math.floor(Math.random() * gameImages.length);
   store.push(random);
-
+  updateScore();
   store.forEach((num, index) => {
     setTimeout(() => {
       gameImages[num].style.animation = "none";
@@ -29,9 +35,7 @@ console.log(store);
 // Click events
 gameImages.forEach((img) => {
   img.addEventListener("click", () => {
-    console.log(img.dataset.id);
-
-    console.log(store[clickCount]);
+    img.style.animation = "intro 0.5s ease-in-out";
     if (store[clickCount] == img.dataset.id) {
       clickCount++;
 
@@ -43,16 +47,36 @@ gameImages.forEach((img) => {
         }, 500);
       }
     } else {
-      score.textContent = `Score: ${score}`;
-      alert("Wrong! Try again.");
-      clickCount = 0;
-      store = [];
-      let random = Math.floor(Math.random() * gameImages.length);
-      store.push(random);
-      continueGame();
+      if (dialog) {
+        dialog.showModal();
+      }
+      finalScoreDisplay.textContent = score;
+      resetGame();
     }
 
     console.log(img);
     console.log(clickCount);
   });
+});
+
+function resetGame() {
+  store = [];
+  score = 0;
+  clickCount = 0;
+  let random = Math.floor(Math.random() * gameImages.length);
+  store.push(random);
+  continueGame();
+}
+
+resetButton.addEventListener("click", resetGame);
+
+function updateScore() {
+  score++;
+  scoreDisplay.textContent = `Score: ${score}`;
+}
+
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+  gameInterval = setInterval(moveSnake, 200);
+  startGame();
 });
